519f10b10a5e36f80bd688d6870e004d2329d1f9
varun-mondee <98588620+varun-mondee@users.noreply.github.com>
2022-01-30 19:05:38 +0530

    Add files via upload

--- JDBCDEMO.java
+++ JDBCDEMO.java
@@ -4,13 +4,20 @@ public class JDBCDEMO {
 
 	
 	public static void main(String[] args) throws Exception{
-	Class.forName("com.mysql.jdbc.Driver");
+	Class.forName("com.mysql.cj.jdbc.Driver");
 	String url = "jdbc:mysql://localhost:3306/employee";
 	String username ="root";
 	String password = "root";
 	Connection con = DriverManager.getConnection(url, username, password);
 	if(con!=null)
-	   System.out.print("connection established");	
+	   System.out.println("connection established");	
+	
+	Statement st = con.createStatement();
+	int i = st.executeUpdate("insert into emp values(4,'michale','hyd',34000,'tester')");
+	if(i != 0)
+		System.out.print("Record Inserted!!");	
+	
 	}
+	
 
 }
